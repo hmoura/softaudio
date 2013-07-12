87fcc8639a60c61abb1bfd4c5854fb252d136cfb
wilzadantas <wilza.dantas@gmail.com>
2013-07-12 12:13:51 -0300

    Mudança no caminho do link do Poral, para abrir em outra aba.

--- app/views/public/_inc_footer.php
+++ app/views/public/_inc_footer.php
@@ -1,7 +1,7 @@
 <div class="container topo">
 	<div id="rodape">
 		<div id="rodape1">
-	            <a  titulo="SoftAudio" href="http://softaudio.com.br/"><img src="http://softaudio.com.br/softaudio/webroot/img/portal-logo.png"></a>
+	            <a  titulo="SoftAudio" href="http://softaudio.com.br/" target="_blank"><img src="http://softaudio.com.br/softaudio/webroot/img/portal-logo.png"></a>
 	  	</div>
 	  	<div id="rodape2">
 	            SoftAudio - Sistema de Avaliação Audiométrica Básica &nbsp;&nbsp;<a  titulo="SoftAudio" href="<?php echo WWWROOT?>"><img src="http://softaudio.com.br/softaudio/webroot/img/logo-mini.png"></a>
